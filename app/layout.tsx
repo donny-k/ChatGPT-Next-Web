ed514e5c7f23331cf6ff985582c5a879e7cf415a
donny-k <61078759+donny-k@users.noreply.github.com>
2024-07-19 14:32:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "NextChat Dnmr.net",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "NextChat Dnmr.net",
     statusBarStyle: "default",
   },
 };
